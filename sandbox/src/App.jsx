1f3256844125b5fe1d9bb91235417520b9d6e70e
SHREYA KHURANA <shreyakhurana1012@gmail.com>
2025-07-16 14:19:30 +0530

    Add files via upload

---
+++ sandbox/src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
